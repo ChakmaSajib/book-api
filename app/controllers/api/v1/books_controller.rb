272bdf5d3406c493ada04cbd6cdd423c640f6dba
ChakmaSajib <sajibchakmahust@gmail.com>
2021-12-16 11:13:17 +0600

    Rails app with Api only

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,33 @@
+module Api
+    module V1  
+        class BooksController < ApplicationController
+            ALLOW_DATA = %[title description rating].freeze
+            def index 
+                books = Book.all
+                render json: books
+            end
+            
+            def show 
+                book = Book.find(params[:id])
+                render json: book
+            end 
+
+            def create 
+                data = json_payload.select { |k| ALLOW_DATA.include?(k) } 
+                book = Book.new(data)
+                if book.save 
+                    render json: book
+                else 
+                    render json: {
+                        "error": "could not create it"
+                    }
+                end 
+            end 
+
+            def destroy
+                book = Book.find(params[:id])
+                book.destroy
+            end 
+        end
+    end 
+end 
